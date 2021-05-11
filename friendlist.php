0599296e3b1bea26def3300c6bbe4bca854aa32d
Adrian Sim Huan Tze <101225244@students.swinburne.edu.my>
2021-05-11 17:09:37 +0800

    Fix friendadd.php and index.php

--- friendlist.php
+++ friendlist.php
@@ -1,34 +1,34 @@
 <?php
     session_start();
     include "system_functions.php";
-    if (!isset($_SESSION['profile_name'])) {
-        header("Location: index.php");
-    }else{
-        $profile_name = $_SESSION['profile_name'];
-    }
+if (!isset($_SESSION['profile_name'])) {
+    header("Location: index.php");
+} else {
+    $profile_name = $_SESSION['profile_name'];
+}
 
-    if (isset($_GET['friend_id'])&& isset($_GET['user_id']) && (!isset($_GET['rate']))){
-        $unfriend_id = $_GET['friend_id'];
-        $user_id = $_GET['user_id'];
-        Unfriend($unfriend_id, $user_id);
-    }
+if (isset($_GET['friend_id'])&& isset($_GET['user_id']) && (!isset($_GET['rate']))) {
+    $unfriend_id = $_GET['friend_id'];
+    $user_id = $_GET['user_id'];
+    Unfriend($unfriend_id, $user_id);
+}
 
-    if (isset($_GET['friend_id'])&& isset($_GET['user_id']) && (isset($_GET['rate']))){
-        $friend_id = $_GET['friend_id'];
-        $user_id = $_GET['user_id'];
-        $rating = $_GET['rate'];
-        Ratefriend($friend_id, $user_id, $rating);
-    }
+if (isset($_GET['friend_id'])&& isset($_GET['user_id']) && (isset($_GET['rate']))) {
+    $friend_id = $_GET['friend_id'];
+    $user_id = $_GET['user_id'];
+    $rating = $_GET['rate'];
+    Ratefriend($friend_id, $user_id, $rating);
+}
 ?>
 
 <!DOCTYPE html>
 
 <html lang="en">
-	<!-- Description: Assignment 2 -->
-	<!-- Author: Adrian Sim Huan Tze -->
-	<!-- Date: 5th November 2020 -->
-	<!-- Validated: 5th November 2020-->
-	
+    <!-- Description: Assignment 2 -->
+    <!-- Author: Adrian Sim Huan Tze -->
+    <!-- Date: 5th November 2020 -->
+    <!-- Validated: 5th November 2020-->
+
     <head>
         <title>MyFriend - Friend List</title>
         <meta charset="utf-8">
@@ -45,23 +45,23 @@
             My Friend System
         </h1>
 
-        <h2><?php echo $profile_name."'s Friend List Page" ?></h2>
+        <h2><?php echo $profile_name . "'s Friend List Page" ?></h2>
 
         <?php
             include "settings.php";
             // Create connection
             $conn = @mysqli_connect($servername, $username, $password, $dbname);
             // Check connection
-            if (!$conn) {
-                die("<p>Connection failed: " . mysqli_connect_error()."</p>"); 
-            }
+        if (!$conn) {
+            die("<p>Connection failed: " . mysqli_connect_error() . "</p>");
+        }
 
             // change default database to '101225244' database
             $dbSelect = @mysqli_select_db($conn, 101225244);
 
-            if(!$dbSelect){
-                die("<p>The database is not available.</p>");
-            }
+        if (!$dbSelect) {
+            die("<p>The database is not available.</p>");
+        }
 
             $sql = "SELECT * FROM users WHERE profile_name =?";
 
@@ -106,7 +106,7 @@
 
             $user_friends = array();
 
-            while ($row) {
+        while ($row) {
                 $sql = "SELECT * FROM users WHERE user_id =?";
 
                 $prepared_stmt = mysqli_prepare($conn, $sql);
@@ -128,7 +128,7 @@
 
                 array_push($user_friends, array($row[0],$record[3],$user_id));
                 $row = mysqli_fetch_row($queryResult);
-            }
+        }
 
             $_SESSION["user_friends"] = $user_friends;
 
@@ -137,7 +137,7 @@
 
         ?>
 
-        <h3><?php echo "Total number of friends is ".$num_of_friends ?></h3>
+        <h3><?php echo "Total number of friends is " . $num_of_friends ?></h3>
 
         <table id="friendlist">
                 <th>
@@ -154,24 +154,24 @@
                     </tr>
                 </th>
                 <?php
-                    if (count($user_friends) == 0) {
+                if (count($user_friends) == 0) {
                         echo "<p>You have not added any friends yet</p>";
-                    }else{
-                        foreach ($user_friends as $key => $value) {
+                } else {
+                    foreach ($user_friends as $key => $value) {
                             echo "<tr>";
-                            echo "<td>".$value[1]."</td>";
-                            echo "<td><a class = 'unfriend' href='friendlist.php?friend_id=".$value[0]."&user_id=".$user_id."'>Unfriend</a></td>";
-                            echo "<td><a href='friendlist.php?rate=1&friend_id=".$value[0]."&user_id=".$user_id."'><img src = 'images/rating1.png' alt = 'rating1'></a><a href='friendlist.php?rate=2&friend_id=".$value[0]."&user_id=".$user_id."'><img src = 'images/rating2.png' alt = 'rating2'></a><a href='friendlist.php?rate=3&friend_id=".$value[0]."&user_id=".$user_id."'><img src = 'images/rating3.png' alt = 'rating3'></a><a href='friendlist.php?rate=4&friend_id=".$value[0]."&user_id=".$user_id."'><img src = 'images/rating4.png' alt = 'rating4'></a><a href='friendlist.php?rate=5&friend_id=".$value[0]."&user_id=".$user_id."'><img src = 'images/rating5.png' alt = 'rating5'></a></td>";
+                            echo "<td>" . $value[1] . "</td>";
+                            echo "<td><a class = 'unfriend' href='friendlist.php?friend_id=" . $value[0] . "&user_id=" . $user_id . "'>Unfriend</a></td>";
+                            echo "<td><a href='friendlist.php?rate=1&friend_id=" . $value[0] . "&user_id=" . $user_id . "'><img src = 'images/rating1.png' alt = 'rating1'></a><a href='friendlist.php?rate=2&friend_id=" . $value[0] . "&user_id=" . $user_id . "'><img src = 'images/rating2.png' alt = 'rating2'></a><a href='friendlist.php?rate=3&friend_id=" . $value[0] . "&user_id=" . $user_id . "'><img src = 'images/rating3.png' alt = 'rating3'></a><a href='friendlist.php?rate=4&friend_id=" . $value[0] . "&user_id=" . $user_id . "'><img src = 'images/rating4.png' alt = 'rating4'></a><a href='friendlist.php?rate=5&friend_id=" . $value[0] . "&user_id=" . $user_id . "'><img src = 'images/rating5.png' alt = 'rating5'></a></td>";
                             echo "</tr>";
-                        }
                     }
+                }
                 ?>
         </table>
 
         <table id="navigation">
             <tbody>
                 <tr>
-                    <td><?php echo "<a class = 'navigate' href='friendadd.php?user_id=".$user_id."&num_of_friends=".$num_of_friends."'><span></span><span></span><span></span><span></span>Add Friends</a>" ?></td>
+                    <td><?php echo "<a class = 'navigate' href='friendadd.php?user_id=" . $user_id . "&num_of_friends=" . $num_of_friends . "'><span></span><span></span><span></span><span></span>Add Friends</a>" ?></td>
                     <td><a class = "navigate" href="logout.php">Log out</a><span></span><span></span><span></span><span></span></td>
                 </tr>
             </tbody>
