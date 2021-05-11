8b51138375ecb7e9913d5e0e630a8d1046058d79
Adrian Sim Huan Tze <101225244@students.swinburne.edu.my>
2021-05-11 12:51:03 +0800

    Fix codes

--- CreateDatabase.php
+++ CreateDatabase.php
@@ -1,3 +1,4 @@
+<div>
 <?php
 // set the servername,username and password
 $servername = "localhost";
@@ -5,23 +6,22 @@ $username = "root";
 $password = "";
 
 // Create connection
-//The mysqli_connect() function attempts to open a connection to the MySQL Server 
+//The mysqli_connect() function attempts to open a connection to the MySQL Server
 $conn = @mysqli_connect($servername, $username, $password);
 
 // Check connection
 if (!$conn) {
-	//The die() function is an alias of the exit() function.
-    die("<p>Connection failed: " . mysqli_connect_error()."</p>"); 
+    //The die() function is an alias of the exit() function
+    die("<p>Connection failed: " . mysqli_connect_error() . "</p>");
 }
-    
+
 echo "<p>Connected database server successfully</p>";
-    
+
 $sql = "CREATE DATABASE `101225244`";
-    
-if(@mysqli_query($conn, $sql)) {
-    false;    
+
+if (@mysqli_query($conn, $sql)) {
+    false;
 }
-
-
 @mysqli_close($conn);
 ?>
+</div>
---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    if (isset($_SESSION['profile_name']) && isset($_SESSION['user_friends'])) {
+        unset($_SESSION['profile_name']);
+        unset($_SESSION['user_friends']);
+        session_unset();
+        session_destroy();
+        header('Location: index.php');
+    }
+    header('Location: index.php');
+    session_destroy();
+?>
