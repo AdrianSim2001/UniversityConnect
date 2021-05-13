43d987eb9a4ca1a3dd771b41bed3ce98f303a5da
Adrian Sim Huan Tze <101225244@students.swinburne.edu.my>
2021-05-13 21:49:03 +0800

    Add new footer for logged-in page

---
+++ footer_login.php
@@ -0,0 +1,33 @@
+<div>
+<?php echo "
+<footer>
+    <a class='footerAnchor' href='index.php'><img src = 'images/companylogo.png' alt='icon'></a>
+    <table id = 'footer'>
+        <thead>
+            <tr>
+                <td>What to do?</td>
+                <td>About Us</td>
+                <td>Partnering with</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><a class='footerAnchor' href='logout.php'>Log Out</a></td>
+                <td><a class='footerAnchor' href = 'mailto:universityconnect@gmail.com'>Contact Us</a></td>
+                <td>
+                    <ul>
+                        <li><img src = 'images/facebook.png' alt='icon'></li>
+                        <li><img src = 'images/instagram.png' alt='icon'></li>
+                        <li><img src = 'images/twitter.png' alt='icon'></li>
+                        <li><img src = 'images/youtube.png' alt='icon'></li>
+                        <li><img src = 'images/whatsapp.png' alt='icon'></li>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <p id = 'footer'>&copy;2020 Greetings! All Rights Reserved</p>
+</footer>"
+?>
+</div>
