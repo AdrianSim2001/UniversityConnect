96872fcf1d1170d2e16dfa169eb10fdbe9be5b09
Adrian Sim Huan Tze <101225244@students.swinburne.edu.my>
2021-05-15 11:33:00 +0800

    Add new tables for comment on discussion

--- CreateTables.php
+++ CreateTables.php
@@ -10,14 +10,14 @@ if (!$conn) {
 }
 
 // sql to create table
-    $sql = "CREATE TABLE IF NOT EXISTS `users`(
-        user_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-        user_email VARCHAR(50) NOT NULL,
-        password VARCHAR(20) NOT NULL,
-        profile_name VARCHAR(30) NOT NULL,
-        date_started DATE NOT NULL,
-        num_of_friends INT UNSIGNED
-        )";
+$sql = "CREATE TABLE IF NOT EXISTS `users`(
+    user_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    user_email VARCHAR(50) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    profile_name VARCHAR(30) NOT NULL,
+    date_started DATE NOT NULL,
+    num_of_friends INT UNSIGNED
+    )";
 
 if (@mysqli_query($conn, $sql)) {
     $sql = "CREATE TABLE IF NOT EXISTS `myfriends`(
@@ -27,9 +27,19 @@ if (@mysqli_query($conn, $sql)) {
         )";
     if (!@mysqli_query($conn, $sql)) {
         die("<p>Failed creating tables: " . mysqli_error($conn) . "</p>");
+    }else {
+        $sql = "CREATE TABLE IF NOT EXISTS `comments`(
+            user_id INT NOT NULL,
+            profile_name VARCHAR(50) NOT NULL,
+            comment VARCHAR(255),
+            post_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP   
+                )";
+        if(!@mysqli_query($conn, $sql)) {
+            die("<p>Failed creating tables: " . mysqli_error($conn) . "</p>");
+        }
     }
-} else {
-        die("<p>Failed creating tables: " . mysqli_error($conn) . "</p>");
+}else {
+    die("<p>Failed creating tables: " . mysqli_error($conn) . "</p>");
 }
 
 @mysqli_close($conn);
