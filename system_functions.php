10c522d7c2fbc16b5b670ef2afa7fecaf3b14e9f
Adrian Sim Huan Tze <101225244@students.swinburne.edu.my>
2021-05-25 15:28:10 +0800

    Modify checkEmailForLogin function to include the features of validate email format and check input

--- system_functions.php
+++ system_functions.php
@@ -70,7 +70,7 @@ function chkEmail($input)
         $email = strval($email);
         $pattern = "/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/";
         if (preg_match($pattern, $email) == 1) {
-            include "settings.php";
+            /*include "settings.php";
             // Create connection
             $conn = @mysqli_connect($servername, $username, $password, $dbname);
             // Check connection
@@ -112,7 +112,8 @@ function chkEmail($input)
                 $emailOK = true;
             }
 
-            @mysqli_close($conn);
+            @mysqli_close($conn);*/
+            $emailOK = true;
         } else {
             $emailOK = false;
             $email_msg = "The email format is not valid. Please try again!";
@@ -182,65 +183,73 @@ function ChkEmailPasswordForLogin($emailInput, $passwordInput)
     $login_password = $passwordInput;
     $login_msg = "";
     $LoginOK = false;
-    $error = "";
+    $error = "email";
+    $profile = "";
 
-    include "settings.php";
-    // Create connection
-    $conn = @mysqli_connect($servername, $username, $password, $dbname);
-    // Check connection
-    if (!$conn) {
-        die("<p>Connection failed: " . mysqli_connect_error() . "</p>");
-    }
+    [$login_msg, $LoginOK, $email] = chkEmail($email);
 
-    // change default database to 'universityconnect' database
-    $dbSelect = @mysqli_select_db($conn, 'universityconnect');
-
-    if (!$dbSelect) {
-        die("<p>The database is not available.</p>");
-    }
-
-    $email = mysqli_escape_string($conn, $email);
-    $login_password = mysqli_escape_string($conn, $login_password);
-    $login_password = substr($login_password, 0, 20);
-
-    // check if the input email already exists or not by getting them from the database
-    $sql = "SELECT * FROM users WHERE user_email = ?";
-
-    $prepared_stmt = @mysqli_prepare($conn, $sql);
-
-    //Bind input variables to prepared statement
-    @mysqli_stmt_bind_param($prepared_stmt, 's', $email);
-
-    //Execute prepared statement
-    @mysqli_stmt_execute($prepared_stmt);
-
-    // Get resultset
-    $queryResult =  @mysqli_stmt_get_result($prepared_stmt)
-        or die("<p>Unable to select from database table</p>");
-
-    // Close the prepared statement
-    @mysqli_stmt_close($prepared_stmt);
-
-    $row = mysqli_num_rows($queryResult);
-
-    if ($row <= 0) {
-        $error = "email";
-        $login_msg = "Cannot find your account. Please register before you login.";
-        $LoginOK = false;
-        $profile = "";
-    } else if ($row == 1) {
-        $profile = $row[3];
-        $row = mysqli_fetch_row($queryResult);
-        if ($row[2] == $login_password) {
-            $LoginOK = true;
-            $profile = $row[3];
-        } else {
-            $error = "password";
-            $login_msg = "Incorrect Password. Please try again.";
-            $LoginOK = false;
+    if($LoginOK){
+        include "settings.php";
+        // Create connection
+        $conn = @mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("<p>Connection failed: " . mysqli_connect_error() . "</p>");
         }
+
+        // change default database to 'universityconnect' database
+        $dbSelect = @mysqli_select_db($conn, 'universityconnect');
+
+        if (!$dbSelect) {
+            die("<p>The database is not available.</p>");
+        }
+
+        $email = mysqli_escape_string($conn, $email);
+        $login_password = mysqli_escape_string($conn, $login_password);
+        $login_password = substr($login_password, 0, 20);
+
+        // check if the input email already exists or not by getting them from the database
+        $sql = "SELECT * FROM users WHERE user_email = ?";
+
+        $prepared_stmt = @mysqli_prepare($conn, $sql);
+
+        //Bind input variables to prepared statement
+        @mysqli_stmt_bind_param($prepared_stmt, 's', $email);
+
+        //Execute prepared statement
+        @mysqli_stmt_execute($prepared_stmt);
+
+        // Get resultset
+        $queryResult =  @mysqli_stmt_get_result($prepared_stmt)
+            or die("<p>Unable to select from database table</p>");
+
+        // Close the prepared statement
+        @mysqli_stmt_close($prepared_stmt);
+
+        $row = mysqli_num_rows($queryResult);
+
+        if ($row <= 0) {
+            $error = "email";
+            $login_msg = "Cannot find your account. Please register before you login.";
+            $LoginOK = false;
+            $profile = "";
+        } else if ($row == 1) {
+            $profile = $row[3];
+            $row = mysqli_fetch_row($queryResult);
+            if ($row[2] == $login_password) {
+                $LoginOK = true;
+                $profile = $row[3];
+            } else {
+                $error = "password";
+                $login_msg = "Incorrect Password. Please try again.";
+                $LoginOK = false;
+            }
+        }
+
+        @mysqli_close($conn);
     }
-    @mysqli_close($conn);
+
+
     return [$login_msg, $LoginOK, $email, $error, $profile];
 }
 
